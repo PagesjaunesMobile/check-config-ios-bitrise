899a24be460c255fec8851d89ef5d69db72ed5da
Michaël Duthilleul <mduthilleul.externe@pagesjaunes.fr>
2016-07-15 17:15:57 +0200

    Check workspace & rules paths

--- step.sh
+++ step.sh
@@ -8,8 +8,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/libs/messages.sh"
 source "$SCRIPT_DIR/libs/rules.sh"
 
-values=$(xcodebuild -workspace $check_workspace -scheme $check_scheme -configuration $check_config -showBuildSettings | tail -n +2 | sed 's/^ *//;s/ *$//')
+if [ ! -d "$check_workspace_path" ]; then
+  msg_error "Workspace not found"
+	exit 1
+fi
 
-msg_info "Reading settings from $check_workspace, scheme $check_scheme, configuration $check_config"
+if [ ! -f "$check_rules_path" ]; then
+  msg_error "Rules not found"
+	exit 1
+fi
+
+values=$(xcodebuild -workspace $check_workspace_path -scheme $check_scheme -configuration $check_config -showBuildSettings | tail -n +2 | sed 's/^ *//;s/ *$//')
+
+msg_info "Reading settings from $check_workspace_path, scheme $check_scheme, configuration $check_config"
 
 source $check_rules_path
